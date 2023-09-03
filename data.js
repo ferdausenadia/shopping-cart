7c56afd930d9ca24e878ea104fa2dfb99536c780
ferdausenadia <itzferdausebd456@gmail.com>
2023-09-03 11:43:37 +0600

    Initial Commit

---
+++ data.js
@@ -0,0 +1,30 @@
+let shopItems = [
+  {
+    id: "0",
+    name: "Speaker",
+    price: "100",
+    desc: "lorem ipsum dolorsit amet consect",
+    img: "images/speaker.jpg",
+  },
+  {
+    id: "1",
+    name: "Pepsi",
+    price: "20",
+    desc: "lorem ipsum dolorsit amet consect",
+    img: "images/pepsi.jpg",
+  },
+  {
+    id: "2",
+    name: "Bag",
+    price: "120",
+    desc: "lorem ipsum dolorsit amet consect",
+    img: "images/bag1.jpg",
+  },
+  {
+    id: "3",
+    name: "Watch",
+    price: "100",
+    desc: "lorem ipsum dolorsit amet consect",
+    img: "images/watch.jpg",
+  },
+];
